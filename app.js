72e76b30340e1503fe5414e992629e556d5f6c32
Pieta9221 <mercy_nwaodu@yahoo.com>
2023-04-19 15:32:28 +0100

    add functionality to page

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
+//jshint esversion:6
+
 const express = require('express')
-const bodyparser = require('body-parser')
+const bodyParser = require('body-parser')
+const date = require(__dirname + '/date.js')
 
 const app = express()
 
+let items = ['Sleep', 'Eat', 'Code']
+let workItems = []
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static('public'))
+
 app.get('/', function(req, res){
-    var today = new Date()
-    var currentDay = today.getDay()
-
-    if(currentDay === 6  || currentDay === 0){
-        res.write('<h1>Yay, its the weekend!</h1>')
-        res.write('<p>I do not need to work</p>')
-        res.send()
-    } else{
-        res.sendFile(__dirname + '/index.html')
-    }
-
+  
+    let day = date()
+   res.render('list', {listTitle: day, newListItems: items})
 })
+
+app.post('/', function(req, res){
+    let item = req.body.newItem
+    if(req.body.list === 'Work List'){
+        workItems.push(item)
+        res.redirect('/work')
+    } else {
+       items.push(item)
+       res.redirect('/') 
+    }   
+    
+})
+
+app.get('/work', function(req, res){
+    res.render('list', {listTitle: 'Work List', newListItems: workItems})
+})
+
+app.get('/about', function(req, res){
+    res.render('about')
+})
+
 
 app.listen(3000, function(){
     console.log('Server started at port 3000')
